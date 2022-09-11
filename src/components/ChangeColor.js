d93be9fa64372ffc81bab9dc0979f5cca85ce6a1
Minsung (Nicholas) Suh <didauds@gmail.com>
2022-09-10 18:53:45 -0600

    Add Redux Toolkit app

---
+++ src/components/ChangeColor.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { changeColor } from "../features/theme";
+
+const ChangeColor = () => {
+  const dispatch = useDispatch();
+  const [color, setColor] = useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setColor(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch(changeColor(color));
+        }}
+      >
+        Change Color
+      </button>
+    </div>
+  );
+};
+
+export default ChangeColor;
---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+const Login = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(
+            login({ name: "Minsung", age: 20, email: "minsung@gmail.com" })
+          );
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Log Out
+      </button>
+    </div>
+  );
+};
+
+export default Login;
